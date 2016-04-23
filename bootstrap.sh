114abf136eb4ae4a8d8b6ac49b333760a758a3bf
Markus John <johm@quyo.de>
2016-04-23 13:08:12 +0200

    Update joe configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 ~/bootstrap.sh/scripts/timezone
+~/bootstrap.sh/scripts/apt
 
 mkdir ~/tmp
 cd ~/tmp
 
-~/bootstrap.sh/scripts/apt
 ~/bootstrap.sh/scripts/openssh
 ~/bootstrap.sh/scripts/joe
 ~/bootstrap.sh/scripts/screen
