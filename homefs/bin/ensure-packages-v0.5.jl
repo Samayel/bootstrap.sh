86a32c3ee99a64e009845422bb7fcbf2936938d9
Markus John <johm@quyo.de>
2017-02-13 15:19:39 +0000

    Add ensure-packages-v0.5.jl

---
+++ homefs/bin/ensure-packages-v0.5.jl
@@ -0,0 +1,45 @@
+#!/usr/bin/env julia
+
+isinitialized = isdir(Pkg.dir())
+
+isinitialized || Pkg.init()
+cd(Pkg.dir())
+
+homedir=ENV["HOME"]
+islink("REQUIRE") || begin
+    rm("REQUIRE")
+    symlink("$homedir/repository/github.com/Samayel/bootstrap.sh/homefs/.julia/v0.5/REQUIRE", "REQUIRE")
+    isinitialized = false
+end
+
+Pkg.update()
+
+withenv("PYTHON" => "") do
+    Pkg.build("PyCall")
+end
+
+isinitialized || begin
+    Pkg.checkout("Nemo")
+    Pkg.build("Nemo")
+
+    Pkg.checkout("Hecke")
+    Pkg.build("Hecke")
+
+    Pkg.checkout("Symata")
+    Pkg.build("Symata")
+end
+
+isdir(joinpath(Pkg.dir(), "PrimeSieve")) || begin
+    Pkg.clone("git@github.com:Samayel/PrimeSieve.jl.git")
+    Pkg.build("PrimeSieve")
+end
+
+isdir(joinpath(Pkg.dir(), "Brainstorm")) || begin
+    Pkg.clone("git@github.com:Samayel/Brainstorm.jl.git")
+    Pkg.build("Brainstorm")
+end
+
+isdir(joinpath(Pkg.dir(), "ProjectEuler")) || begin
+    Pkg.clone("git@github.com:Samayel/ProjectEuler.jl.git")
+    Pkg.build("ProjectEuler")
+end
