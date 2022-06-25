41c682f4362c224dd652ada22fd031620e0bc078
Markus John <johm@quyo.de>
2022-06-25 08:12:21 +0000

    Fix duply script

--- scripts/duply.nix.sh
+++ scripts/duply.nix.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -I nixpkgs=/root/.nix-defexpr/channels/nixpkgs-22_05
+#! nix-shell -I nixpkgs=/root/.nix-defexpr/channels/nixpkgs
 #! nix-shell -i bash -p duply pkgs.python39Packages.boto
 
 exec -a "$0" duply "$@"
